7c38c577d937f3f766c16facfc476ff158d2ec6c
JohnYezub <buze@ya.ru>
2020-11-17 20:07:52 +0800

    Initial Commit

---
+++ NewsFeed/NewsFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedApp.swift
+//  NewsFeed
+//
+//  Created by   admin on 17.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct NewsFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
